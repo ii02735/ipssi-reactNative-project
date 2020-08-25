21d0855793e442f40f128b0d98801c5f3bb7ea2e
ii02735 <bilaal.yadallee@gmail.com>
2020-08-25 17:35:41 +0200

    [MAIN] Composant principal : navigation WIP

--- App.tsx
+++ App.tsx
@@ -1,15 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import "react-native-gesture-handler";
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeView from "./src/views/HomeView";
+import ScannerView from "./src/views/ScannerView";
+import ResultsView from "./src/views/ResultsView";
 
-export default function App() {
+const App = ():JSX.Element => {
+
+  const Stack = createStackNavigator();
+  /**
+   * Mise en place de la navigation inter-écrans
+   */
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+     <NavigationContainer>
+       <Stack.Navigator>
+          <Stack.Screen name="Accueil" component={HomeView} />
+          <Stack.Screen name="Scanner" component={ScannerView}/>
+          <Stack.Screen name="Résultats" component={ResultsView} />
+       </Stack.Navigator>
+     </NavigationContainer>
+  )
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
