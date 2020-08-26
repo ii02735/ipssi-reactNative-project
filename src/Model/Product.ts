ef820d975104e66413726221f7f5c0d5cee1d1bd
ii02735 <bilaal.yadallee@gmail.com>
2020-08-26 08:42:46 +0200

    [MODEL] Schéma produit

---
+++ src/Model/Product.ts
@@ -0,0 +1,14 @@
+export default interface Product {
+    nom: string, //generic_name_fr
+    marque: string, //brands
+    ciqual: string, //composition nutritionnelle d'un aliment au vu de la table Ciqual, category_properties[ciqual_food_name:fr]
+    barcode: string, //pour effectuer recherche, ou depuis l'historique, ou afficher dans résultats
+    pays_vente: string, //countries
+    magasins_vente: string, //stores
+    pays_producteur: string, //manufacturing_places
+    ingredients: string, //ingredients_text
+    creation_time: string, //depuis entry_dates_tags : récupérer premier élément
+    image_url: string, //image_front_url
+    etiquettes: string, //labels
+    keywords: string[], //keywords
+}
