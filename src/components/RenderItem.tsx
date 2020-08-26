80278cdf488e4dcd9b626fd7fb3600086ab5036c
Yadallee Bilaal <bilaal.yadallee@insign.fr>
2020-08-27 01:09:44 +0200

    [VIEW] Ajout date ajout produit favori

--- src/components/RenderItem.tsx
+++ src/components/RenderItem.tsx
@@ -16,7 +16,7 @@ const RenderItem = ({ navigation, route, product }:{ navigation: any, route: any
     const details = 
 `#### **${product.nom}**
 - Identifiant code-barres : **${product.barcode}**
-- Date d'ajout : ${product.dateFavoris} 
+- Date d'ajout : ${product.dateFavori} 
 `;
     
     return (<Div key={product.barcode} style={{ borderColor: "black", borderWidth: 1, padding: 30, marginBottom: 50 }}>
