5d3d6252b38a445c1caf46cbe4deb8283dada1ff
Yadallee Bilaal <bilaal.yadallee@insign.fr>
2020-08-27 18:48:02 +0200

    [CORE] Attachement de l'historique

--- App.tsx
+++ App.tsx
@@ -1,26 +1,32 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, HeaderBackButton } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeView from "./src/views/HomeView";
 import ScannerView from "./src/views/ScannerView";
 import ResultsView from "./src/views/ResultsView";
 import IngredientsView from "./src/views/IngredientsView";
 import favoriteProductsReducer from "./store/favoriteProducts/reducers";
+import HistoryProductsReducer from "./store/searchedProduct/reducers";
 import errorsReducer from "./store/errors/reducers";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import FavoriteView from "./src/views/FavoriteView";
+import HistoryView from "./src/views/HistoryView";
+import { Ionicons } from '@expo/vector-icons';
 
 const store = createStore(combineReducers({
   favoriteProducts: favoriteProductsReducer,
-  errors: errorsReducer
+  errors: errorsReducer,
+  searchedProducts: HistoryProductsReducer
 }));
 
 const App = ():JSX.Element => {
 
   const Stack = createStackNavigator();
+  const Tab = createBottomTabNavigator();
   /**
    * Mise en place de la navigation inter-écrans
    */
@@ -34,6 +40,7 @@ const App = ():JSX.Element => {
           <Stack.Screen name="Données produit"  component={ResultsView} />
           <Stack.Screen name="Ingrédients" component={IngredientsView} />
           <Stack.Screen name="Produits favoris" component={FavoriteView} />
+          <Stack.Screen name="Historique" component={HistoryView} />
        </Stack.Navigator>
      </NavigationContainer>
      </Provider>
